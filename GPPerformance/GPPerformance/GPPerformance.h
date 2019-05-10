322bdfd7d7862cabf88f9d759c30c410384fe858
Liyanwei <yifan.lyw@alibaba-inc.com>
2019-05-10 15:18:56 +0800

    修改文件目录

---
+++ GPPerformance/GPPerformance/GPPerformance.h
@@ -0,0 +1,19 @@
+//
+//  GPPerformance.h
+//  GPPerformance
+//
+//  Created by Liyanwei on 2019/5/10.
+//  Copyright © 2019 Liyanwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GPPerformance.
+FOUNDATION_EXPORT double GPPerformanceVersionNumber;
+
+//! Project version string for GPPerformance.
+FOUNDATION_EXPORT const unsigned char GPPerformanceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPPerformance/PublicHeader.h>
+
+
